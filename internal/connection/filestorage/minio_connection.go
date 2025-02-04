a24eb82b47b087ab894cd0c4bd284b067b1f81bf
lukedge86 <l.delbue@studenti.unisa.it>
2025-02-04 15:31:09 +0100

    feat:  add MinIO connection handling with custom option
    - Implemented MinioConnection to manage connections to a MinIO server.
    - Added CreateMinioConnection and NewMinIOConnection functions to support authentication via credentials or environment variables.
    - Created example to demonstrate different connection methods.

---
+++ internal/connection/filestorage/minio_connection.go
@@ -0,0 +1,70 @@
+package connfilestorage
+
+import (
+	"context"
+	"fmt"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+	"m2cs/internal/connection"
+	"os"
+)
+
+// MinioConnection represents a connection to a MinIO server.
+type MinioConnection struct {
+	client *minio.Client
+	connection.Properties
+}
+
+// GetClient returns the MinIO client.
+func (m *MinioConnection) GetClient() *minio.Client {
+	return m.client
+}
+
+// CreateMinioConnection creates a new MinioConnection.
+// It takes an endpoint, an AuthConfig, and optional MinIO options.
+// It returns a MinioConnection or an error if the connection could not be established.
+func CreateMinioConnection(endpoint string, config *connection.AuthConfig, minioOptions *minio.Options) (*MinioConnection, error) {
+
+	if minioOptions == nil {
+		minioOptions = &minio.Options{
+			Secure: false,
+		}
+	}
+
+	switch config.GetConnectType() {
+	case "withCredential":
+		if config.GetAccessKey() == "" || config.GetSecretKey() == "" {
+			return nil, fmt.Errorf("access key and/or secret key not set")
+		}
+		minioOptions.Creds = credentials.NewStaticV4(config.GetAccessKey(), config.GetSecretKey(), "")
+	case "withEnv":
+		accessKey := os.Getenv("MINIO_ACCESS_KEY")
+		secretKey := os.Getenv("MINIO_SECRET_KEY")
+		if accessKey == "" || secretKey == "" {
+			return nil, fmt.Errorf("environment variables MINIO_ACCESS_KEY and/or MINIO_SECRET_KEY are not set")
+		}
+		minioOptions.Creds = credentials.NewStaticV4(accessKey, secretKey, "")
+
+	default:
+		return nil, fmt.Errorf("invalid connection type: %s", config.GetConnectType())
+	}
+
+	conn := &MinioConnection{
+		Properties: config.GetProperties(),
+	}
+
+	minioClient, err := minio.New(endpoint, minioOptions)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to create MinIO client: %w", err)
+	}
+
+	_, err = minioClient.ListBuckets(context.Background())
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to MinIO: %w", err)
+	}
+
+	conn.client = minioClient
+	return conn, nil
+}
---
+++ m2cs-api-connection.go
@@ -0,0 +1,64 @@
+package m2cs
+
+import (
+	"fmt"
+	"github.com/minio/minio-go/v7"
+	"m2cs/internal/connection"
+	connfilestorage "m2cs/internal/connection/filestorage"
+)
+
+// ConnectionOptions holds the options for creating a connection.
+// parameters:
+// - ConnectionMethod: The method used to establish the connection.
+// - IsMainInstance:Indicates if this is the main instance.
+// - SaveEncrypt:  Indicates if the data should be saved with encryption.
+// - SaveCompress: Indicates if the data should be saved with compression.
+type ConnectionOptions struct {
+	ConnectionMethod connectionFunc
+	IsMainInstance   bool
+	SaveEncrypt      bool
+	SaveCompress     bool
+}
+
+type connectionFunc *connection.AuthConfig
+
+// NewMinIOConnection creates a new MinIO connection.
+// It takes an endpoint, connection options, and optional MinIO options.
+// It returns a MinioConnection or an error if the connection could not be established.
+func NewMinIOConnection(endpoint string, connectionOptions ConnectionOptions, minioOptions *minio.Options) (*connfilestorage.MinioConnection, error) {
+	var authConfing *connection.AuthConfig = connectionOptions.ConnectionMethod
+
+	if authConfing.GetConnectType() != "withCredential" && authConfing.GetConnectType() != "withEnv" {
+		return nil, fmt.Errorf("invalid connection method for MinIO; use: ConnectWithCredentials or ConnectWithEnvCredentials")
+	}
+
+	authConfing.SetProperties(connection.Properties{
+		IsMainInstance: connectionOptions.IsMainInstance,
+		SaveEncrypted:  connectionOptions.SaveEncrypt,
+		SaveCompressed: connectionOptions.SaveCompress,
+	})
+
+	minioConn, err := connfilestorage.CreateMinioConnection(endpoint, authConfing, minioOptions)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return minioConn, nil
+}
+
+// ConnectWithCredentials returns a connectionFunc configured with the provided credentials.
+func ConnectWithCredentials(identity string, secretAccessKey string) connectionFunc {
+	authConfig := connection.NewAuthConfig() // Usa la funzione per creare l'oggetto
+	authConfig.SetConnectType("withCredential")
+	authConfig.SetAccessKey(identity)
+	authConfig.SetSecretKey(secretAccessKey)
+	return authConfig
+}
+
+// ConnectWithEnvCredentials returns a connectionFunc configured to use environment credentials.
+func ConnectWithEnvCredentials() connectionFunc {
+	authConfig := &connection.AuthConfig{}
+	authConfig.SetConnectType("withEnv")
+	return authConfig
+}
